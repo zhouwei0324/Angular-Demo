1fc976dad49a05b00fdc21ebff1bb64ef0ccd82c
zhouwei0324 <15617412260@163.com>
2023-04-04 11:05:43 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ButtonModule } from 'ng-devui/button';
+import { AccordionModule } from 'ng-devui/accordion';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Demo';
+
+    ngOnInit(): void {
+
+    }
+
+    husband() {
+        alert(1);
+    }
 }
---
+++ src/app/component/huanba/huanba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-huanba',
+  templateUrl: './huanba.component.html',
+  styleUrls: ['./huanba.component.less']
+})
+export class HuanbaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
